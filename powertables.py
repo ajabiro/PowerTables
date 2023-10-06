99d82c13518f8d05e7470dfb0fd2bdbcd3667e7f
Aranda <ajabiro1211@gmail.com>
2023-10-05 21:52:16 -0400

    first commit

---
+++ powertables.py
@@ -0,0 +1,28 @@
+while True:
+    userNum = int(input("Enter an integer: "))
+
+    print("Number")
+    print("======")
+    for n in range(0, userNum):
+        n += 1
+        print(n)
+
+    print("Squared")
+    print("=======")
+    for n in range(0, userNum):
+        n += 1
+        print(n * n)
+
+    print("Cubed")
+    print("=====")
+    for n in range(0, userNum):
+        n += 1
+        print(n * n * n)
+
+    keepGoing = input("Would you like to continue? (y/n)")
+    if keepGoing == "y":
+        continue
+    elif keepGoing == "n":
+        print("Goodbye!")
+        break
+
